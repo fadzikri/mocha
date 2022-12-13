dc73c73a6fe340888d1a22220783fa5a66512209
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-13 07:48:00 +0700

    Fix JSON

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ const app = express()
 const port = 3000
 
 app.use(favicon(`${__dirname}/favicon.ico`));
-app.use(express.json())
 
 app.get('/', (req, res) => {
-  console.dir(req.headers)
-  res.json({
-    "message": "Hello World!"
-  });
+  res.json(req.headers);
 })
 
 app.listen(port, () => {
